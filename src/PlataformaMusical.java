d066e891a32267fcb5628e5d802d17fdc9d83520
Claudio Junior <100850489+ClaudioJJ@users.noreply.github.com>
2022-03-10 13:40:47 -0300

    Update PlataformaMusical.java

--- src/PlataformaMusical.java
+++ src/PlataformaMusical.java
@@ -9,25 +9,25 @@ public class PlataformaMusical {
 		int musica1 = 0;
 		int musica2 = 0;
 		int musica3 = 0;
-		System.out.println("Digite o número da música que quer ouvir de 1 a 3 ou 4 para finalizar: ");
+		System.out.println("Digite o nÃºmero da mÃºsica que quer ouvir de 1 a 3 ou 4 para finalizar: ");
 		int tipo = sc.nextInt();
 
 		while (tipo != 4) {
 			if (tipo == 1) {
 				musica1 += 1;
-				System.out.println("Você ouviu a música1");
+				System.out.println("VocÃª ouviu a mÃºsica1");
 			} else if (tipo == 2) {
 				musica2 += 1;
-				System.out.println("Você ouviu a música2");
+				System.out.println("VocÃª ouviu a mÃºsica2");
 			} else if (tipo == 3) {
 				musica3 += 1;
-				System.out.println("Você ouviu a música3");
+				System.out.println("VocÃª ouviu a mÃºsica3");
 			}
 
 			tipo = sc.nextInt();
 		}
-		System.out.println("Obrigado por utilizar nossa plataforma de música4");
-		System.out.println("O número de ouvintes das muúsicas foram: ");
+		System.out.println("Obrigado por utilizar nossa plataforma de mÃºsica");
+		System.out.println("O nÃºmero de ouvintes das muÃºsicas foram: ");
 		System.out.println("musica1: " + musica1);
 		System.out.println("musica2: " + musica2);
 		System.out.println("musica3: " + musica3);
